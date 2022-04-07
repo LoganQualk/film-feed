333cb98184a1c0ee112b4e378e59bd4b41498562
Simon Post <postse@live.unc.edu>
2022-04-07 13:19:04 -0400

    Added emojis to posts

--- src/Feed.js
+++ src/Feed.js
@@ -1,10 +1,49 @@
 import Post from "./components/Post";
 
+// Temp data - replace with firebase
+const posts = [
+    {
+        "user": "John Doe",
+        "date": new Date(), // replace with date posted
+        "attachedMovies": [
+            "Batman Begins",
+            "The Dark Knight"
+        ],
+        "text": "Which of these movies is yall's favorite?  I just watched both and I think I prefer The Dark Knight.",
+        "reactions": {
+            "heart": 0,
+            "laugh": 0,
+            "cry": 0,
+            "surprise": 0,
+            "clap": 0,
+            "thumbsDown": 0
+        },
+        "replies": [
+            {
+                "user": "Mary Jane",
+                "date": new Date(), // replace with time/date replied,
+                "replies": [
+                    {
+                        "user": "Bingo Boffman",
+                        "date": new Date(), // replace with time/date replied,
+                        "replies": []
+                    }
+                ]
+            },
+            {
+                "user": "Jonah Babona",
+                "date": new Date(), // replace with time/date replied,
+                "replies": []
+            }
+        ]
+    }
+]
+
 const Feed = () => {
     return ( 
         <div className="container">
             <h1>Feed</h1>
-            <Post></Post>
+            {posts.map((post, index) => <Post key={index} data={post}></Post>)}
         </div>
      );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,18 +1,33 @@
-const Post = () => {
+// Emojis used from sensa emoji
+import heartEmoji from '../images/heart.svg'
+import cryingLaughingEmoji from '../images/cryingLaughing.svg'
+import clapEmoji from '../images/clap.svg'
+import sobEmoji from '../images/sob.svg'
+import surpriseEmoji from '../images/surprise.svg'
+import thumbsDownEmoji from '../images/thumbsDown.svg'
+
+const Post = ({data}) => {
     return (
         <div className="post">
             <div className="postContent">
                 <div>
-                    <h2>John Doe</h2>
+                    <h2>{data.user}</h2>
                 </div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur dolor consectetur eaque porro quos corrupti atque quidem accusantium voluptate deserunt minus animi a debitis enim tempore earum reprehenderit, magni excepturi.</p>
+                <p>{data.text}</p>
             </div>
             <div className="postActions">
-                <div>
-                    Reactions go here
+                <div className="reactionEmojis">
+                    <img className='reactionEmoji' src={heartEmoji} alt="Heart Emoji" />
+                    <img className='reactionEmoji' src={cryingLaughingEmoji} alt="Crying Laughing Emoji" />
+                    <img className='reactionEmoji' src={clapEmoji} alt="Clap Emoji" />
+                    <img className='reactionEmoji' src={sobEmoji} alt="Sob Emoji" />
+                    <img className='reactionEmoji' src={surpriseEmoji} alt="Surprise Emoji" />
+                    <img className='reactionEmoji' src={thumbsDownEmoji} alt="Thumbs Down Emoji" />
                 </div>
-                <div>
-                    Comment box goes here
+                <hr />
+                <div className="commentAction">
+                    <input type="text" name="commentInput" className="commentInput" placeholder="Type a comment..."/>
+                    <button className="defaultButton bg-quaternary">Comment</button>
                 </div>
             </div>
         </div>
