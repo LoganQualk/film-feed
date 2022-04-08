3f9219a85c9c9c9b9a6b6ed1dc3c270771a4a665
Simon Post <postse@live.unc.edu>
2022-04-08 14:01:39 -0400

    Reactions are now selectable

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,8 +5,17 @@ import clapEmoji from '../images/clap.svg'
 import sobEmoji from '../images/sob.svg'
 import surpriseEmoji from '../images/surprise.svg'
 import thumbsDownEmoji from '../images/thumbsDown.svg'
+import { useState } from 'react'
 
 const Post = ({ data }) => {
+    // Need to make it so when they are selected it actually changes the data, 
+    // figure I'd wait for firebase to be set up for that
+    const [heartEmojiSelected, setHeartEmojiSelected] = useState(false);
+    const [cryingLaughingEmojiSelected, setCryingLaughingEmojiSelected] = useState(false);
+    const [clapEmojiSelected, setClapEmojiSelected] = useState(false);
+    const [sobEmojiSelected, setSobEmojiSelected] = useState(false);
+    const [surpriseEmojiSelected, setSurpriseEmojiSelected] = useState(false);
+    const [thumbsDownEmojiSelected, setThumbsDownEmojiSelected] = useState(false);
 
     const displayComments = (comments, level) => {
         // Recursively gets comments from top layer to bottom
@@ -30,7 +39,7 @@ const Post = ({ data }) => {
                 <div className="postContent">
                     <h2>{data.user}</h2>
                     <div className={data.attachedMovies.length > 1 ? "flexCol" : "flexRow"}>
-                        <div className='flexRow'>
+                        <div className='flexRow wrap'>
                             {data.attachedMovies.map((movie, index) =>
                                 <div key={index} className='posterContainer'>
                                     <img src={movie.imageUrl} alt={movie.name + " Poster"} />
@@ -43,12 +52,30 @@ const Post = ({ data }) => {
                 </div>
                 <div className="postActions">
                     <div className="reactionEmojis">
-                        <img className='reactionEmoji' src={heartEmoji} alt="Heart Emoji" />
-                        <img className='reactionEmoji' src={cryingLaughingEmoji} alt="Crying Laughing Emoji" />
-                        <img className='reactionEmoji' src={clapEmoji} alt="Clap Emoji" />
-                        <img className='reactionEmoji' src={sobEmoji} alt="Sob Emoji" />
-                        <img className='reactionEmoji' src={surpriseEmoji} alt="Surprise Emoji" />
-                        <img className='reactionEmoji' src={thumbsDownEmoji} alt="Thumbs Down Emoji" />
+                        <button className={'reactionEmojiContainer' + (heartEmojiSelected ? " emojiSelected" : "")} onClick={() => setHeartEmojiSelected(!heartEmojiSelected)}>
+                            <img className='reactionEmoji' src={heartEmoji} alt="Heart Emoji" />
+                            <p>5</p>
+                        </button>
+                        <button className={'reactionEmojiContainer' + (cryingLaughingEmojiSelected ? " emojiSelected" : "")} onClick={() => setCryingLaughingEmojiSelected(!cryingLaughingEmojiSelected)}>
+                            <img className='reactionEmoji' src={cryingLaughingEmoji} alt="Crying Laughing Emoji" />
+                            <p>5</p>
+                        </button>
+                        <button className={'reactionEmojiContainer' + (clapEmojiSelected ? " emojiSelected" : "")} onClick={() => setClapEmojiSelected(!clapEmojiSelected)}>
+                            <img className='reactionEmoji' src={clapEmoji} alt="Clap Emoji" />
+                            <p>5</p>
+                        </button>
+                        <button className={'reactionEmojiContainer' + (sobEmojiSelected ? " emojiSelected" : "")} onClick={() => setSobEmojiSelected(!sobEmojiSelected)}>
+                            <img className='reactionEmoji' src={sobEmoji} alt="Sob Emoji" />
+                            <p>5</p>
+                        </button>
+                        <button className={'reactionEmojiContainer' + (surpriseEmojiSelected ? " emojiSelected" : "")} onClick={() => setSurpriseEmojiSelected(!surpriseEmojiSelected)}>
+                            <img className='reactionEmoji' src={surpriseEmoji} alt="Surprise Emoji" />
+                            <p>5</p>
+                        </button>
+                        <button className={'reactionEmojiContainer' + (thumbsDownEmojiSelected ? " emojiSelected" : "")} onClick={() => setThumbsDownEmojiSelected(!thumbsDownEmojiSelected)}>
+                            <img className='reactionEmoji' src={thumbsDownEmoji} alt="Thumbs Down Emoji" />
+                            <p>5</p>
+                        </button>
                     </div>
                     <hr />
                     <div className="commentAction">
