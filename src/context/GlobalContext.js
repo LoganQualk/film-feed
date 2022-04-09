62fa6e5d7cd535e1ec87abc4f19c9553f1966882
LoganQualk <loganq@gmail.com>
2022-04-09 02:32:53 -0400

    added changePage to createContext

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -1,6 +1,9 @@
 import { createContext } from "react";
 import { useNavigate } from "react-router-dom";
-export const GlobalContext = createContext({});
+
+export const GlobalContext = createContext({
+    changePage: () => {}
+});
 
 export const GlobalProvider = ({ children }) => {
 
