c90acbb7bd7d9b9d687f51edf04c263f59d946da
Simon Post <postse@live.unc.edu>
2022-04-09 15:18:22 -0400

    Implemented function to take logs and sort by date in diary

--- src/Diary.js
+++ src/Diary.js
@@ -1,9 +1,24 @@
+import React from "react";
+import logs from "./tempData/logs";
+
 const Diary = () => {
-    return ( 
+    const logData = Object.entries(logs);
+
+    return (
         <div>
             <h1>Diary</h1>
+            {logData.sort((logA, logB) => logB[1].date - logA[1].date).map(log =>
+                <React.Fragment>
+                    <div className="bg-tertiary">
+                        <h2>{log[1].title}</h2>
+                        <p>{log[1].text}</p>
+                        <p>Logged on {log[1].date.toLocaleDateString()}</p>
+                    </div>
+                    <br />
+                </React.Fragment>
+            )}
         </div>
-     );
+    );
 }
- 
+
 export default Diary;
--- src/tempData/logs.js
+++ src/tempData/logs.js
@@ -1,6 +1,25 @@
 const logs = {
     "2": {
-        "date": new Date(),
+        "title": "The Dark Knight 1",
+        "date": new Date('4/5/2020'),
+        "text": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse ex ad cupiditate adipisci ut illo suscipit vitae doloremque asperiores. Doloremque quos repellendus, iusto omnis voluptatibus impedit eaque laboriosam. Earum, sit.",
+        "rating": 4.5
+    },
+    "3": {
+        "title": "The Dark Knight 2",
+        "date": new Date('4/8/2020'),
+        "text": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse ex ad cupiditate adipisci ut illo suscipit vitae doloremque asperiores. Doloremque quos repellendus, iusto omnis voluptatibus impedit eaque laboriosam. Earum, sit.",
+        "rating": 4.5
+    },
+    "4": {
+        "title": "The Dark Knight 3",
+        "date": new Date('4/3/2020'),
+        "text": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse ex ad cupiditate adipisci ut illo suscipit vitae doloremque asperiores. Doloremque quos repellendus, iusto omnis voluptatibus impedit eaque laboriosam. Earum, sit.",
+        "rating": 4.5
+    },
+    "5": {
+        "title": "The Dark Knight 4",
+        "date": new Date('4/7/2020'),
         "text": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse ex ad cupiditate adipisci ut illo suscipit vitae doloremque asperiores. Doloremque quos repellendus, iusto omnis voluptatibus impedit eaque laboriosam. Earum, sit.",
         "rating": 4.5
     }
