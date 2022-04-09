a607ed7070ee1e864b35d209b7f2d227d768337b
LoganQualk <loganq@gmail.com>
2022-04-08 23:50:27 -0400

    finished login page, added spans with colors and onClick to homepage

--- src/Login.js
+++ src/Login.js
@@ -1,18 +1,34 @@
 const Login = () => {
-    return ( 
+
+    const handleClick = () => {
+        window.history.pushState({}, undefined, "/");
+    }
+
+    return (
         <div className="loginPage flexCol justifyCenter alignCenter">
             <h1>Welcome to FilmFeed</h1>
-            <form className="flexCol">
+            <p className="flexCol alignCenter">
+                <div>
+                    <span className="light-purple-text">Log movies in your diary for your eyes only. </span>
+                    <span className="orange-text">Write posts about movies to keep your friends updated. </span>
+                    <span className="green-text">Make reviews for the world to see your critical perspective. </span>
+                </div>
+                <span className="light-blue-text">FilmFeed is the place for all your movie-discussing needs.</span>
+            </p>
+            <form className="flexCol alignCenter">
                 <label className="flexCol">
-                    <input type="text" name="username" placeholder="Username" />
-                    <input type="text" name="password" placeholder="Password" />
+                    <input className="loginField" type="text" name="username" placeholder="Username" />
+                    <br />
+                    <input className="loginField" type="text" name="password" placeholder="Password" />
+                    <br />
                 </label>
-                <input type="submit" value="Log-in"/>
-                <input className="bg-tertiary" type="submit" value="Sign-up" />
+                <button className="loginButton" onClick={() => handleClick()}>Log-in</button>
+                <br />
+                <input className="bg-tertiary loginButton" type="submit" value="Sign-up" />
             </form>
             <h3>Forgot Password?</h3>
         </div>
-     );
+    );
 }
- 
+
 export default Login;
