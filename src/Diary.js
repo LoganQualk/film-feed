d4433e45326716f9b5027e7bf0aaa325e3f54208
Advaith Deo <you@example.com>
2022-04-19 02:50:36 -0400

    changes to diary rendering

--- src/Diary.js
+++ src/Diary.js
@@ -3,71 +3,151 @@ import { useContext, useState } from "react";
 import logs from "./tempData/logs";
 import Header from './components/Header';
 import LogMovieModal from './components/LogMovieModal'
+import { GlobalContext } from "./context/GlobalContext";
+const monthDict = {0:'January',
+                    1: 'February',
+                    2: 'March',
+                    3: 'April',
+                    4: 'May',
+                    5: 'June',
+                    6: 'July',
+                    7: 'August',
+                    8: 'September',
+                    9: 'October',
+                    10: 'November',
+                    11: 'December'};
 
 const Diary = () => {
     const logData = Object.entries(logs);
-    const months = []; // track the months already in diary
-    for(let i = 0; i < logData.length; i++){
-        if(!months.includes(logData[i][1].date.getMonth())){
-            months.push(logData[i][1].date.getMonth());
+    const months = [1]; // track the months already in diary
+    const updateMonths = () => {
+        for(let i = 0; i < logData.length; i++){
+            if(!months.includes(logData[i][1].date.getMonth())){
+                months.push(logData[i][1].date.getMonth());
+                let month = monthDict[logData[i][1].date.getMonth()];
+                let monthYear = month + " " + logData[i][1].date.getFullYear()
+                let monthDiv = document.createElement("div")
+                monthDiv.innerHTML = `<div id="` + month + `" class="month-header">
+                                        <p>` + monthYear + `</p>
+                                    </div>
+                                    <div id="` + month + logData[i][1].date.getFullYear() + `" class="month-movies">
+                                    </div>`;
+                
+                let diaryContent = document.getElementById("diaryContent");
+                diaryContent.prepend(monthDiv);
+            }
+        }
+        for (var i in logData){
+            let entry = ``
+            let log = logData[i][1];
+            let monthYear = monthDict[log.date.getMonth()] + log.date.getFullYear()
+            let month = document.getElementById(monthYear);
+            let stars = ``;
+            
+            for(let i = 1; i <= log.rating; i++){
+                stars += `<div class="clip-star-active"></div>`;
+            }
+            
+            if(log.rating < 5){
+                for(let i = 1; i <= 5-log.rating; i++){
+                    stars += `<div class="clip-star-inactive"></div>`;
+                }
+            }
+            
+            let hasReview = ``;
+            if(log.text != null){
+                hasReview = `<i class="fa fa-align-left"></i>`;
+            }
+            
+            entry = `<div class="diary-entry">
+                            <div class="day">
+                            ` + log.date.getDate() + `
+                            </div>
+                            <div>
+                                <img class = "poster" src="`+ log.imageUrl +`"/>
+                            </div>
+                            <div class="title">
+                                <em>` + log.title + `</em> `+ log.year +`
+                            </div>
+                            <div id="stars">
+                                ` + stars + `
+                            </div>
+                            <div class="rewatch">
+                                <i class="material-icons">replay</i>
+                            </div>
+                            <div class="review">
+                                ` + hasReview + `
+                            </div>
+                            <span class="fa-stack">
+                                <i class='fas fa-pencil-alt fa-stack-2x'></i>
+                                <i class="fa fa-trash-o fa-stack-1x"></i>
+                            </span>
+                        </div>`;
+            if(entry != null && month != null){
+                month.innerHTML += entry;
+            }                    
         }
     }
+    
 
     const sortDiary = () => {
     }
     const logMovie = () =>{
     }
 
-    const render = () =>{
-        let entry = ``
-    let month = document.getElementById("month");
-        
-    for (var i in logData){
-        let log = logData[i][1];
-        let stars = " ";
-        for(let i = 1; i <= log.rating; i++){
-            stars.concat(' ', '<div className="clip-star-active"></div>');
-        }
-        if(log.rating < 5){
-            for(let i = 1; i <= 5-log.rating; i++){
-                stars.concat(' ', '<div className="clip-star-inactive"></div>');
+    /*const render = () =>{
+        for (var i in logData){
+            let entry = ``
+            let log = logData[i][1];
+            let monthYear = monthDict[log.date.getMonth()] + log.date.getFullYear()
+            let month = document.getElementById(monthYear);
+            console.log(monthYear);
+            let stars = ``;
+            
+            for(let i = 1; i <= log.rating; i++){
+                stars += `<div class="clip-star-active"></div>`;
             }
+            
+            if(log.rating < 5){
+                for(let i = 1; i <= 5-log.rating; i++){
+                    stars += `<div class="clip-star-inactive"></div>`;
+                }
+            }
+            
+            let hasReview = ``;
+            if(log.text != null){
+                hasReview = `<i class="fa fa-align-left"></i>`;
+            }
+            
+            entry = `<div class="diary-entry">
+                            <div class="day">
+                            ` + log.date.getDate() + `
+                            </div>
+                            <div>
+                                <img class = "poster" src="`+ log.imageUrl +`"/>
+                            </div>
+                            <div class="title">
+                                <em>` + log.title + `</em> `+ log.year +`
+                            </div>
+                            <div id="stars">
+                                ` + stars + `
+                            </div>
+                            <div class="rewatch">
+                                <i class="material-icons">replay</i>
+                            </div>
+                            <div class="review">
+                                ` + hasReview + `
+                            </div>
+                            <span class="fa-stack">
+                                <i class='fas fa-pencil-alt fa-stack-2x'></i>
+                                <i class="fa fa-trash-o fa-stack-1x"></i>
+                            </span>
+                        </div>`;
+            if(entry != null){
+                month.innerHTML += entry;
+            }                    
         }
-        
-        let hasReview = ``;
-        if(log.text != null){
-            hasReview = `<i class="fa fa-align-left"></i>`;
-        }
-        
-        entry = `<div class="diary-entry">
-                        <div class="day">
-                        ` + log.date.getDate() + `
-                        </div>
-                        <div>
-                            <img class = "poster" src="`+ log.imageUrl +`"/>
-                        </div>
-                        <div class="title">
-                            <em>` + log.title + `</em> `+ log.year +`
-                        </div>
-                        <div id="stars">
-                            ` + stars + `
-                        </div>
-                        <div class="rewatch">
-                            <i class="material-icons">replay</i>
-                        </div>
-                        <div class="review">
-                            ` + hasReview + `
-                        </div>
-                        <span class="fa-stack">
-                            <i class='fas fa-pencil-alt fa-stack-2x'></i>
-                            <i class="fa fa-trash-o fa-stack-1x"></i>
-                        </span>
-                    </div>`;
-        if(entry != null){
-            month.innerHTML += entry;
-        }                    
-    }
-    }
+    }*/
 
     return (
         <>
@@ -92,12 +172,12 @@ const Diary = () => {
                 </div>
                 <input type="text" placeholder="Search for movies"/> 
             </div>
-            <div className="diary-content">
+            <div id="diaryContent" className="diary-content" onLoad={() => updateMonths()}>
                 <div>
                     <div className="month-header">
                         <p>February 2022</p>
                     </div>
-                    <div id="month" className="month-movies" onLoad={() => render()}>
+                    <div id="February2022" className="month-movies">
                         <div className="diary-entry">
                             <div className="day">
                                 20
--- src/tempData/logs.js
+++ src/tempData/logs.js
@@ -1,7 +1,7 @@
 const logData = {
     "2": {
         "title": "The Dark Knight 1",
-        "date": new Date('4/5/2020'),
+        "date": new Date('2/3/2022'),
         "text": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse ex ad cupiditate adipisci ut illo suscipit vitae doloremque asperiores. Doloremque quos repellendus, iusto omnis voluptatibus impedit eaque laboriosam. Earum, sit.",
         "rating": 1,
         "year": 2008,
@@ -9,7 +9,7 @@ const logData = {
     },
     "3": {
         "title": "The Dark Knight 2",
-        "date": new Date('4/8/2020'),
+        "date": new Date('3/4/2022'),
         "text": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse ex ad cupiditate adipisci ut illo suscipit vitae doloremque asperiores. Doloremque quos repellendus, iusto omnis voluptatibus impedit eaque laboriosam. Earum, sit.",
         "rating": 2,
         "year": 2008,
@@ -17,7 +17,7 @@ const logData = {
     },
     "4": {
         "title": "The Dark Knight 3",
-        "date": new Date('4/3/2020'),
+        "date": new Date('4/14/2022'),
         "text": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse ex ad cupiditate adipisci ut illo suscipit vitae doloremque asperiores. Doloremque quos repellendus, iusto omnis voluptatibus impedit eaque laboriosam. Earum, sit.",
         "rating": 4,
         "year": 2008,
@@ -25,7 +25,7 @@ const logData = {
     },
     "5": {
         "title": "The Dark Knight 4",
-        "date": new Date('4/7/2020'),
+        "date": new Date('2/17/2022'),
         "text": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse ex ad cupiditate adipisci ut illo suscipit vitae doloremque asperiores. Doloremque quos repellendus, iusto omnis voluptatibus impedit eaque laboriosam. Earum, sit.",
         "rating": 3,
         "year": 2008,
