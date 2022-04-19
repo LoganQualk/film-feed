cd598abb6a5be196285415ded289d27a1d22f514
Charlotte Wang <charlottewang2000@gmail.com>
2022-04-19 13:22:53 -0400

    each search result on search results page is clickable to specific movie page

--- src/Results.js
+++ src/Results.js
@@ -2,13 +2,13 @@ import { useContext } from "react";
 import { GlobalContext } from "./context/GlobalContext";
 
 const Results = () => {
-    const { changePage, allResults, setMovieId, userSearchInput } = useContext(GlobalContext);
+    const { changePage, allResults, setMovieId, userSearchInput, setIdAndLoad } = useContext(GlobalContext);
 
     return (
         <>
             <div id="listPage" className="containerWithBackground">
                 <div className="flexRow alignCenter">
-                    <div>Search Results for "{userSearchInput}"...</div> {console.log(JSON.stringify(allResults))}
+                    <div className="searchResultsFor">Search Results for "{userSearchInput}"...</div> {console.log(JSON.stringify(allResults))}
                 </div>
                 <hr />
                 {
@@ -16,10 +16,10 @@ const Results = () => {
                         {
                             let posterUrl = `https://image.tmdb.org/t/p/original/${movieObj.poster_path}`;
                             return(
-                        <button key={movieObj.id} className="searchResultsObj">
+                        <button key={movieObj.id} className="searchResultsObj" onClick={() => {setIdAndLoad(movieObj.id)}}>
                             <img className="resultsPoster" src={posterUrl} alt={movieObj.title + " Poster"} />
                             <div className="flexCol alignStart">
-                                <h2>{movieObj.title}</h2>
+                                <div className="searchTitle">{movieObj.title}</div>
                             </div>
                         </button>);
                         }
