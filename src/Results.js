2b135773b38489077e22b3712a6acbc086dfb2c6
Charlotte Wang <charlottewang2000@gmail.com>
2022-04-19 13:33:41 -0400

    added ratings to each search results movie

--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,12 @@
 import { useContext } from "react";
 import { GlobalContext } from "./context/GlobalContext";
+import ReactStars from "react-rating-stars-component";
+
+const compute5StarRating = (value) => {
+    const step = 0.5;
+    let inv = 1.0 / step;
+    return Math.round(parseInt(value) * inv) / (inv * 2.0);
+};
 
 const Results = () => {
     const { changePage, allResults, setMovieId, userSearchInput, setIdAndLoad } = useContext(GlobalContext);
@@ -18,8 +25,22 @@ const Results = () => {
                             return(
                         <button key={movieObj.id} className="searchResultsObj" onClick={() => {setIdAndLoad(movieObj.id)}}>
                             <img className="resultsPoster" src={posterUrl} alt={movieObj.title + " Poster"} />
-                            <div className="flexCol alignStart">
+                            <div className="flexCol alignStart searchInfo">
                                 <div className="searchTitle">{movieObj.title}</div>
+                                <div> 
+                                    <ReactStars
+                                        count={5}
+                                        size={24}
+                                        isHalf={true}
+                                        value={compute5StarRating(movieObj.vote_average)}
+                                        edit={false}
+                                        emptyIcon={<i className="far fa-star"></i>}
+                                        halfIcon={<i className="fa fa-star-half-alt"></i>}
+                                        fullIcon={<i className="fa fa-star"></i>}
+                                        activeColor="#ffd700"
+                                    />
+                                </div>
+                                <div className="searchDirector">Directed by: </div>
                             </div>
                         </button>);
                         }
