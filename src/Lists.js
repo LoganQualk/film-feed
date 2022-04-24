a313cedbdc228464bb281e208bcd8d453621a3cc
Simon Post <postse@live.unc.edu>
2022-04-24 17:54:44 -0400

    now shows blank spaces for when there aren't 3 movies in a list

--- src/Lists.js
+++ src/Lists.js
@@ -9,10 +9,10 @@ const Lists = () => {
 
     const responsive = {
         desktop: {
-          breakpoint: { max: 3000, min: 0 },
-          items: 3,
+            breakpoint: { max: 3000, min: 0 },
+            items: 3,
         }
-      };
+    };
 
     return (
         <>
@@ -28,28 +28,38 @@ const Lists = () => {
                 </div>
                 <hr />
                 {
-                    lists.map((list, index) =>
-                        <div key={index} className="list">
-                            {/* {list.attachedMovies.map((movie, movieIndex) =>
+                    lists.map((list, index) => {
+                        const blankPostersNeeded = Math.max(3 - list.attachedMovies.length, 0);
+                        console.log(blankPostersNeeded);
+                        let blankPosters = [];
+                        for (let i = 0; i < blankPostersNeeded; i++) {
+                            blankPosters.push(0)
+                        }
+
+                        return (
+                            <div key={index} className="list">
+                                {/* {list.attachedMovies.map((movie, movieIndex) =>
                                 <img key={movieIndex} className="listPoster" src={movie.imageUrl} alt={movie.name + " Poster"} />
                             )} */}
-                            <Carousel shouldResetAutoplay={false} containerClass="carousel" sliderClass="carousel" itemClass="carouselImage" responsive={responsive}>
-                                {list.attachedMovies.map((movie, movieIndex) =>
-                                    <img key={movieIndex} className="listPoster" src={movie.imageUrl} alt={movie.name + " Poster"} />
-                                )}
-                            </Carousel>
-                            <div className="flexCol justifyBetween grow">
-                                <div>
-                                    <h2 className="pointer" onClick={() => changePage("lists=" + list.name, list)}>{list.name}</h2>
-                                    <p className="listDescription">{list.description}</p>
+                                <Carousel shouldResetAutoplay={false} containerClass="carousel" sliderClass="carousel" itemClass="carouselImage" responsive={responsive}>
+                                    {list.attachedMovies.map((movie, movieIndex) =>
+                                        <img key={movieIndex} className="listPoster" src={movie.imageUrl} alt={movie.name + " Poster"} />
+                                    )}
+                                    {blankPosters.map((blankPoster, index) => <div className="listPoster" key={index}></div>)}
+                                </Carousel>
+                                <div className="flexCol justifyBetween grow">
+                                    <div>
+                                        <h2 className="pointer" onClick={() => changePage("lists=" + list.name, list)}>{list.name}</h2>
+                                        <p className="listDescription">{list.description}</p>
+                                    </div>
+                                    <p><em>Created on {new Date(list.date).toLocaleDateString()}</em></p>
                                 </div>
-                                <p><em>Created on {new Date(list.date).toLocaleDateString()}</em></p>
+                                <div className="flexCol justifyEnd">
+                                    <button className="defaultButton bg-alarm" onClick={() => setLists(lists.filter(listToDelete => listToDelete.id !== list.id))}>Delete</button>
+                                </div>
                             </div>
-                            <div className="flexCol justifyEnd">
-                                <button className="defaultButton bg-alarm" onClick={() => setLists(lists.filter(listToDelete => listToDelete.id !== list.id))}>Delete</button>
-                            </div>
-                        </div>
-                    )
+                        )
+                    })
                 }
             </div>
         </>
