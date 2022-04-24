3678928d9435bf832435bc43e7fdc1228faddb08
Charlotte Wang <charlottewang2000@gmail.com>
2022-04-23 20:35:05 -0400

    added following/followers to friendsData

--- src/tempData/friends.js
+++ src/tempData/friends.js
@@ -2,50 +2,74 @@ const friendsData = [
     {
         "name": "John Doe",
         "username": "doenut-reviews",
+        "following": 5,
+        "followers": 6,
     },
     {
         "name": "Mary Jane",
         "username": "marysmovies",
+        "following": 291,
+        "followers": 62,
     },
     {
         "name": "Bingo Boffman",
         "username": "bingo_was_his_name",
+        "following": 101,
+        "followers": 111,
     },
     {
         "name": "Jonah Babona",
         "username": "notjonahhill",
+        "following": 249,
+        "followers": 294,
     },
     {
         "name": "Jason Mimoa",
         "username": "aquaman",
+        "following": 10,
+        "followers": 19348,
     },
     {
         "name": "John Stamos",
         "username": "best-uncle",
+        "following": 459,
+        "followers": 1903,
     },
     {
         "name": "Dwayne Johnson",
         "username": "the-rock",
+        "following": 5,
+        "followers": 390506,
     },
     {
         "name": "Zach Braff",
         "username": "braffster",
+        "following": 51,
+        "followers": 634,
     },
     {
         "name": "Roy Williams",
         "username": "beatcoachk",
+        "following": 50,
+        "followers": 63050,
     },
     {
         "name": "Jonah Hill",
         "username": "officalJonah",
+        "following": 21,
+        "followers": 34908,
     },
     {
         "name": "Keanu Reeves",
         "username": "marysmovies",
+        "following": 530,
+        "followers": 6294,
     },
     {
         "name": "Stevie Wonder",
         "username": "wonderman",
+        "following": 29,
+        "followers": 623499,
     },
 ];
 
