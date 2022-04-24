0428a5e511a3f563cc8f76f3ee8831d6e4f2b7d7
Charlotte Wang <charlottewang2000@gmail.com>
2022-04-24 13:22:04 -0400

    delete button works for rec movie modal for attached items

--- src/components/AttachedModalItem.js
+++ src/components/AttachedModalItem.js
@@ -1,14 +1,29 @@
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalContext";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 const AttachedModalItem = ({ id }) => {
+    const { attachedModalItems, setAttachedModalItems } = useContext(GlobalContext);
+
+    const handleDelete = () => {
+        setAttachedModalItems(
+            attachedModalItems.filter((item) => {
+                if(item === id) {
+                    return false;
+                }
+                return true;
+            })
+        );
+    }
+
     return (
         <div className="attachedModalContainer">
             <div className="attachedtoModal">
                 <span className="userCircleModal"><AccountCircleIcon /></span>
                 {id}
                 <span className="deleteButtonContainerModal">
-                    <button className="deleteButton"><DeleteIcon /></button>
+                    <button className="deleteButton" onClick={handleDelete}><DeleteIcon /></button>
                 </span>
             </div>
         </div>
