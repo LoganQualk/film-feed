925e39cb8084d3d04fa75c8ae09a2c18c758317d
Charlotte Wang <charlottewang2000@gmail.com>
2022-04-23 20:28:23 -0400

    added user's friends data

---
+++ src/tempData/friends.js
@@ -0,0 +1,52 @@
+const friendsData = [
+    {
+        "name": "John Doe",
+        "username": "doenut-reviews",
+    },
+    {
+        "name": "Mary Jane",
+        "username": "marysmovies",
+    },
+    {
+        "name": "Bingo Boffman",
+        "username": "bingo_was_his_name",
+    },
+    {
+        "name": "Jonah Babona",
+        "username": "notjonahhill",
+    },
+    {
+        "name": "Jason Mimoa",
+        "username": "aquaman",
+    },
+    {
+        "name": "John Stamos",
+        "username": "best-uncle",
+    },
+    {
+        "name": "Dwayne Johnson",
+        "username": "the-rock",
+    },
+    {
+        "name": "Zach Braff",
+        "username": "braffster",
+    },
+    {
+        "name": "Roy Williams",
+        "username": "beatcoachk",
+    },
+    {
+        "name": "Jonah Hill",
+        "username": "officalJonah",
+    },
+    {
+        "name": "Keanu Reeves",
+        "username": "marysmovies",
+    },
+    {
+        "name": "Stevie Wonder",
+        "username": "wonderman",
+    },
+];
+
+export default friendsData
