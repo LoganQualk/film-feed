179d999df540f28f86f08c419d1c0f8c965f4357
LoganQualk <loganq@gmail.com>
2022-04-25 20:49:43 -0400

    changed the settings page, changed the CSS and there is now a sign out button

--- src/Settings.js
+++ src/Settings.js
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import Header from "./components/Header";
+import { GlobalContext } from "./context/GlobalContext";
 
 const Settings = () => {
+
+    const { changePage } = useContext(GlobalContext);
+
     return (
         <React.Fragment>
             <Header />
-            <div className="fullPage flexCol justifyCenter alignCenter">
+            <div className="fullPage flexCol alignCenter">
                 <h1>Settings</h1>
                 <form>
                     <label>
-                        <div className="flexRow"><p>Change User Name</p><input type="text" name="changeUsername" placeholder="Current Username" /></div>
-                        <div className="flexRow"><p>Change Real Name</p><input type="text" name="changeRealName" placeholder="Current Real Name" /></div>
-                        <div className="flexRow"><p>Change Email</p><input type="text" name="changeEmail" placeholder="Current Email" /></div>
-                        <div className="flexRow"><p>Change Password</p><input type="text" name="currentPassword" placeholder="Enter current password" /></div>
-                        <div><input type="text" name="newPassword" placeholder="Enter New Password" /></div>
-                        <div><p>Default Post Visibility</p></div>
+                        <div className="flexRow marginBottomSmall"><p className="pSetWidth">Change User Name</p><input type="text" name="changeUsername" placeholder="Current Username" /></div>
+                        <div className="flexRow marginBottomSmall"><p className="pSetWidth">Change Real Name</p><input type="text" name="changeRealName" placeholder="Current Real Name" /></div>
+                        <div className="flexRow marginBottomSmall"><p className="pSetWidth">Change Email</p><input type="text" name="changeEmail" placeholder="Current Email" /></div>
+                        <div className="flexRow marginBottomSmall"><p className="pSetWidth">Change Password</p><input type="text" name="currentPassword" placeholder="Enter current password" /></div>
+                        <div className="flexRow justifyEnd marginBottomSmall"><input type="text" name="newPassword" placeholder="Enter New Password" /></div>
+                        <div><p>Default Post Visibility: Public</p></div>
                     </label>
                 </form>
+                <button className="defaultButton bg-tertiary" onClick={() => changePage("login")}>Sign-Out</button>
             </div>
         </React.Fragment>
 
